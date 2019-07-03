032b01ddd1ae5c57334c3e055d4d1451753a87cb
Aparna-VK <aparnav@qburst.com>
2019-07-03 14:18:51 +0530

    done some tasks in golang

---
+++ goExamples/dataTypes.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main() {
+    fmt.Println("go" + "lang")
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("8.0/5.0 =", 8.0/5.0)
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
